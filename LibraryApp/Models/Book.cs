204ef34eb4f7f796a472b44c143a993d51aa4157
Michael Keaveny <michaelkeaveny0@gmail.com>
2020-07-11 20:48:00 +0100

    Recreated the Book Index view

--- LibraryApp/Models/Book.cs
+++ LibraryApp/Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,17 +10,27 @@ namespace LibraryApp.Models
     public class Book
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 2)]
+        [Required]
         public string Title { get; set; }
 
+        [StringLength(60, MinimumLength = 6)]
+        [RegularExpression(@"^[a-zA-Z]+ [a-zA-Z]*$")]
+        [Required]
         public string Author { get; set; }
 
+        [Required]
         public string Genre { get; set; }
 
+        [Required]
         public int Pages { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
+        [Required]
         public bool FinishedReading { get; set; }
     }
 }
